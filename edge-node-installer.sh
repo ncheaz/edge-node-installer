aa27f15752383d2ee6025c36afe2b01c1948280c
Stevan Nesovic <stevannesovic22@gmail.com>
2025-02-04 21:57:55 +0100

    Add required python packages

--- edge-node-installer.sh
+++ edge-node-installer.sh
@@ -213,7 +213,8 @@ sudo apt update
 sudo apt install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncurses5-dev libncursesw5-dev xz-utils tk-dev \
-libffi-dev liblzma-dev python3-openssl git
+libffi-dev liblzma-dev python3-openssl git \
+libmysqlclient-dev pkg-config python3-dev
 
 # Step 2: Install pyenv
 curl https://pyenv.run | bash
